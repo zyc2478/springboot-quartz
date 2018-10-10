3197b7bc9ba21b5f4ad80f283c9641eaf6723683
zyc2478 <zyc2478@126.com>
2018-10-10 14:39:58 +0800

    第一版完整的springboot可运行的模块，http://localhost:8080/test/date 可以成功看到时间

---
+++ springboot-api/src/main/java/com/zyc/springbootapi/SpringbootApiApplication.java
@@ -0,0 +1,12 @@
+package com.zyc.springbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApiApplication.class, args);
+    }
+}
